4a012eb77c6cada5f0f52449ac199877c681bcf9
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2021-11-23 01:02:30 +0100

    fix routing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,16 +6,17 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
         <!-- Styles -->
-        <link rel="preload" href="{{ asset('fonts/SayWhatYouFeel-Condensed.woff2?62dceb4610d64c62b47f9001585abe65') }}" as="font" type="font/woff2" crossorigin>
-        <link rel="preload" href="{{ asset('fonts/KolektivGroteskX-Bold.woff2?0af042f2f88e9aaceaf92d73348ce4f5') }}" as="font" type="font/woff2" crossorigin>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Scripts -->
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="antialiased bg-yellow font-bold font-sans text-base">
+    <body class="font-sans antialiased">
         @inertia
 
         @env ('local')
--- routes/web.php
+++ routes/web.php
@@ -8,20 +8,24 @@ use App\Models\Exhibition;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/exhibitions/{id}', function ($id) {
-    $exhibition = Exhibition::with('cabinets.media')->findOrFail($id);
-    $resource = new ExhibitionResource($exhibition);
-    return Inertia::render('Frontend/Exhibition', compact('resource'));
-})->name('exhibitions.show');
 
-Route::get('/cabinets/{id}', function ($id) {
-    $cabinet = Cabinet::findOrFail($id);
-    return Inertia::render('Frontend/Cabinet', compact('cabinet'));
-})->name('cabinets.show');
+Route::middleware('inertia:frontend')
+    ->group(function () {
+        Route::get('/exhibitions/{id}', function ($id) {
+            $exhibition = Exhibition::with('cabinets.media')->findOrFail($id);
+            $resource = new ExhibitionResource($exhibition);
+            return Inertia::render('Frontend/Exhibition', compact('resource'));
+        })->name('exhibitions.show');
 
-Route::get('/items/{id}', function ($id) {
-    return Inertia::render('Frontend/Item', compact('id'));
-})->name('items.show');
+        Route::get('/cabinets/{id}', function ($id) {
+            $cabinet = Cabinet::findOrFail($id);
+            return Inertia::render('Frontend/Cabinet', compact('cabinet'));
+        })->name('cabinets.show');
+
+        Route::get('/items/{id}', function ($id) {
+            return Inertia::render('Frontend/Item', compact('id'));
+        })->name('items.show');
+    });
 
 Route::middleware('admin', 'inertia:admin')
     ->prefix('admin')
