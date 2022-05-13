ed1523f04948df622e2cf81e8827b8c9c0d688db
Ernest Walzel <eronisko@users.noreply.github.com>
2022-05-13 11:26:20 +0000

    WysiwygEditor component (using VueQuill) WEBUMENIA-1821

---
+++ resources/js/Jetstream/WysiwygEditor.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="shadow-sm">
+        <QuillEditor
+            v-bind="$attrs"
+            contentType="html"
+            theme="snow"
+            :toolbar="['bold', 'italic', 'underline', 'clean']"
+            :content="modelValue"
+            @update:content="$emit('update:modelValue', $event)"
+        />
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+
+export default defineComponent({
+    components: {
+        QuillEditor
+    },
+    props: ['modelValue'],
+
+    emits: ['update:modelValue'],
+})
+</script>
